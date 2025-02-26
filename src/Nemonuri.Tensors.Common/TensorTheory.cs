47c27fb1ff423f3253803900b927d03577f84bbf
nemonuri <nemonuri2@gmail.com>
2025-02-26 20:36:44 +0900

    add SetIndexesToSuccessor

--- src/Nemonuri.Tensors.Common/TensorTheory.cs
+++ src/Nemonuri.Tensors.Common/TensorTheory.cs
@@ -16,38 +16,56 @@ public static class TensorTheory
         Guard.IsEqualTo(startIndexes.Length, source.Rank);
         Guard.IsEqualTo(endIndexes.Length, source.Rank);
 
-        Span<nint> cutIndexes = stackalloc nint[startIndexes.Length];
+        Span<nint> curIndexes = stackalloc nint[startIndexes.Length];
+        startIndexes.CopyTo(curIndexes);
 
+        projectedCount = 0;
         while (true)
         {
-            AdjustIndexes(source.Rank - 1, 1, cutIndexes, source.Lengths, out isFinished);
-            if (isFinished)
+            //destination[projectedCount] = 
+
+            
+            //AdjustIndexes(source.Rank - 1, 1, curIndexes, source.Lengths, out bool overflowed);
+            SetIndexesToSuccessor(curIndexes, source.Lengths, out bool overflowed);
+            
+
+            if (overflowed)
             {
                 break;
             }
+
+            projectedCount++;
         }
         
+    }
+
+    public static void SetIndexesToSuccessor(Span<nint> indexes, scoped ReadOnlySpan<nint> lengths, out bool overflowed)
+    {
+        AdjustIndexes(indexes.Length - 1, 1, indexes, lengths, out overflowed);
     }
 
 /**
 Source:
 https://github.com/dotnet/runtime/blob/main/src/libraries/System.Numerics.Tensors/src/System/Numerics/Tensors/netcore/TensorSpanHelpers.cs
 */
-    public static void AdjustIndexes(int curIndex, nint addend, Span<nint> curIndexes, scoped ReadOnlySpan<nint> length, out bool isFinished)
+    public static void AdjustIndexes(int curIndex, nint addend, Span<nint> curIndexes, scoped ReadOnlySpan<nint> lengths, out bool overflowed)
     {
-        isFinished = false;
-
-        if (addend <= 0 || curIndex < 0)
+        if (addend <= 0)
         {
-            isFinished = true;
+            overflowed = false;
+            return;
+        }
+        if (curIndex < 0)
+        {
+            overflowed = true;
             return;
         }
             
         curIndexes[curIndex] += addend;
 
-        (nint Quotient, nint Remainder) result = Math.DivRem(curIndexes[curIndex], length[curIndex]);
+        (nint Quotient, nint Remainder) result = Math.DivRem(curIndexes[curIndex], lengths[curIndex]);
 
-        AdjustIndexes(curIndex - 1, result.Quotient, curIndexes, length, out isFinished);
+        AdjustIndexes(curIndex - 1, result.Quotient, curIndexes, lengths, out overflowed);
         curIndexes[curIndex] = result.Remainder;
     }
 }
